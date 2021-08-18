d2ab5a8c3b12a220069ff2405b401b3e3bca5875
softhank <68536803+softhank@users.noreply.github.com>
2021-08-18 16:33:56 +0900

    Create solution.dart

---
+++ Layout Widget/solution.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body:const Align(
+      alignment: Alignment.topLeft,
+      child: Image(
+        image: NetworkImage('https://images.unsplash.com/photo-1549124151-844d2a02fe9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1188&q=80'),
+        width: 400,
+          ),
+        ),
+      ),
+    );
+  }
+}
