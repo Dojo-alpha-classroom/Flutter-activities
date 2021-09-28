a5080df5d1cf61ec58c04bf23c4fda7f23a2ae9f
softhank <68536803+softhank@users.noreply.github.com>
2021-09-28 15:53:19 +0900

    Update main.dart

--- Gesture-Detector-Exercise/main.dart
+++ Gesture-Detector-Exercise/main.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter layout demo',
+        home: Scaffold(
+          body: MyBox(),
+        ));
+  }
+}
+
+class MyBox extends StatefulWidget {
+  const MyBox({Key? key}) : super(key: key);
+
+  @override
+  _MyBoxState createState() => _MyBoxState();
+}
+
+class _MyBoxState extends State<MyBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightBlueAccent,
+        ),
+        child: Text("Change Color"),
+      ),
+    );
+  }
+}
