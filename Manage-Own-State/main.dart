1f8f9dee590f2f0198c236472ee61542a3baf787
softhank <68536803+softhank@users.noreply.github.com>
2021-10-04 15:53:05 +0900

    Update main.dart

--- Manage-Own-State/main.dart
+++ Manage-Own-State/main.dart
@@ -1,46 +1,51 @@
+import 'package:flutter/material.dart';
+
 void main() {
- runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Interactive Flutter Demo',
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Interactive Flutter Demo'),
-       ),
-       body: Center(
-         child: MyButton(),
-       ),
-     ),
-   );
- }
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Interactive Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Interactive Flutter Demo'),
+        ),
+        body: const Center(
+          child: MyButton(),
+        ),
+      ),
+    );
+  }
 }
 
 class MyButton extends StatefulWidget {
- MyButton({Key key}) : super(key: key);
+  const MyButton({Key? key}) : super(key: key);
 
- @override
- _MyButtonState createState() => _MyButtonState();
+  @override
+  _MyButtonState createState() => _MyButtonState();
 }
 
 class _MyButtonState extends State<MyButton> {
- bool _active = false;
+  bool _active = false;
 
- void _handleTap() {
-   setState(() {
-     _active = !_active;
-   });
- }
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
 
- Widget build(BuildContext context) {
-   return GestureDetector(
-     onTap: _handleTap,
-     child: Container(
-         //add some contents that depend on the state of _active
-     ),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        //add some contents that depend on the state of _active
+      ),
+    );
+  }
 }
