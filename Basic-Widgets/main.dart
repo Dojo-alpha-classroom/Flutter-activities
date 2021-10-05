b158cf448d9d59e28191a7eee4ea44a33167bb10
softhank <68536803+softhank@users.noreply.github.com>
2021-10-05 10:51:23 +0900

    Create main.dart

---
+++ Basic-Widgets/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.grey,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
