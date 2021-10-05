a355837533aede45dbcc7e8f85ce4afa084561ce
softhank <68536803+softhank@users.noreply.github.com>
2021-10-05 11:29:17 +0900

    Update main.dart

--- Non-Material/main.dart
+++ Non-Material/main.dart
@@ -10,15 +10,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(color: Colors.white),
-      child: const Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
+      child: Column(
+        children: [
+          Container(
+            height: 56.0, // in logical pixels
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            decoration: BoxDecoration(color: Colors.blue[500]),
+            // Row is a horizontal, linear layout.
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                  "Flutter layout demo",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ),
-        ),
+          Expanded(
+            child: const Center(
+              child: Text(
+                'Hello World',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
